104573b43edd6f009eb391f13a8879531ab2a71b
Johan Cosemans <johan.cosemans@lab900.com>
2020-05-11 21:15:54 +0200

    Updated folder structure and readme

---
+++ projects/angular-admin/src/lib/angular-admin.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { AngularAdminComponent } from './angular-admin.component';
+
+
+
+@NgModule({
+  declarations: [AngularAdminComponent],
+  imports: [
+  ],
+  exports: [AngularAdminComponent]
+})
+export class AngularAdminModule { }
---
+++ projects/angular-admin/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of angular-admin
+ */
+
+export * from './lib/angular-admin.service';
+export * from './lib/angular-admin.component';
+export * from './lib/angular-admin.module';
