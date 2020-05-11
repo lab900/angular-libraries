282b786c3c21ee8de4827e20bef96171ff0f1f89
Johan Cosemans <johan.cosemans@lab900.com>
2020-05-11 21:14:13 +0200

    Updated folder structure and readme

--- angularAdminLibrary/projects/angular-admin/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of angular-admin
- */
-
-export * from './lib/angular-admin.service';
-export * from './lib/angular-admin.component';
-export * from './lib/angular-admin.module';
