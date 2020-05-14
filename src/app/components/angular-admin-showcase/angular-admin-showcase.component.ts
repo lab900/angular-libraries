bdea1fccfa4736e46729a4e79b1eadf240149065
Johan Cosemans <johan.cosemans@lab900.com>
2020-05-14 15:29:16 +0200

    Table styling

--- src/app/components/angular-admin-showcase/angular-admin-showcase.component.ts
+++ src/app/components/angular-admin-showcase/angular-admin-showcase.component.ts
@@ -60,7 +60,7 @@ export class AngularAdminShowcaseComponent implements OnInit {
         title: 'Content',
         attribute: 'content',
         editType: EditType.Wysiwyg,
-        showInOverview: false
+        showInOverview: true
       },
       {
         title: 'Background',
@@ -85,6 +85,7 @@ export class AngularAdminShowcaseComponent implements OnInit {
           title: 'hello world',
           subTitle: 'hello world sub',
           author: 'Johan',
+          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
           postedBy: 'johan',
           postedOn: new Date(),
           postedOnShort: new Date(),
