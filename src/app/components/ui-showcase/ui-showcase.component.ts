c4b945a464347aa523dceb457506f3c524fc681f
Johan Cosemans <johan.cosemans@lab900.com>
2020-05-23 23:56:38 +0200

    Added example for alert dialog

--- src/app/components/ui-showcase/ui-showcase.component.ts
+++ src/app/components/ui-showcase/ui-showcase.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Alert } from '../../../../projects/ui/src/lib/alert/models/alert';
+import { MatDialog } from '@angular/material/dialog';
+import { AlertDialogComponent } from '../../../../projects/ui/src/lib/dialog/components/alert-dialog/alert-dialog.component';
 
 @Component({
   selector: 'lab900-ui-showcase',
@@ -12,11 +14,20 @@ export class UiShowcaseComponent implements OnInit {
   public readonly Error = Alert.Error;
   public readonly Success = Alert.Success;
 
-  constructor() {}
+  constructor(public dialog: MatDialog) {}
 
   ngOnInit(): void {}
 
   log(message: string) {
     console.log(message);
+  }
+
+  alert(message: string) {
+    this.dialog.open(AlertDialogComponent, {
+      data: {
+        message: message,
+        okButtonText: 'Thanks!',
+      },
+    });
   }
 }
