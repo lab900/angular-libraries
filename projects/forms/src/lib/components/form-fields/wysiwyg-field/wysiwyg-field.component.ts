d834c4245c22d10257dd076d68f031c3a35b322e
Johan Cosemans <johan.cosemans@lab900.com>
2020-05-26 21:01:42 +0200

    Added default config in wysiwyg editor

--- projects/forms/src/lib/components/form-fields/wysiwyg-field/wysiwyg-field.component.ts
+++ projects/forms/src/lib/components/form-fields/wysiwyg-field/wysiwyg-field.component.ts
@@ -16,45 +16,48 @@ export class WysiwygFieldComponent extends FormComponent implements OnInit {
   ngOnInit(): void {
     this.editorConfig = {
       editable: true,
-      spellcheck: true,
-      height: 'auto',
-      minHeight: '0',
-      maxHeight: 'auto',
-      width: 'auto',
-      minWidth: '0',
-      translate: 'yes',
-      enableToolbar: true,
-      showToolbar: true,
-      placeholder: 'Enter text here...',
-      defaultParagraphSeparator: '',
-      defaultFontName: '',
-      defaultFontSize: '',
-      fonts: [
-        { class: 'arial', name: 'Arial' },
-        { class: 'times-new-roman', name: 'Times New Roman' },
-        { class: 'calibri', name: 'Calibri' },
-        { class: 'comic-sans-ms', name: 'Comic Sans MS' },
-      ],
-      customClasses: [
-        {
-          name: 'quote',
-          class: 'quote',
-        },
-        {
-          name: 'redText',
-          class: 'redText',
-        },
-        {
-          name: 'titleText',
-          class: 'titleText',
-          tag: 'h1',
-        },
-      ],
-      uploadUrl: 'v1/image',
-      uploadWithCredentials: false,
       sanitize: true,
-      toolbarPosition: 'top',
-      toolbarHiddenButtons: [['bold', 'italic'], ['fontSize']],
+
+      /*      editable: true,
+            spellcheck: true,
+            height: 'auto',
+            minHeight: '0',
+            maxHeight: 'auto',
+            width: 'auto',
+            minWidth: '0',
+            translate: 'yes',
+            enableToolbar: true,
+            showToolbar: true,
+            placeholder: 'Enter text here...',
+            defaultParagraphSeparator: '',
+            defaultFontName: '',
+            defaultFontSize: '',
+            fonts: [
+              { class: 'arial', name: 'Arial' },
+              { class: 'times-new-roman', name: 'Times New Roman' },
+              { class: 'calibri', name: 'Calibri' },
+              { class: 'comic-sans-ms', name: 'Comic Sans MS' },
+            ],
+            customClasses: [
+              {
+                name: 'quote',
+                class: 'quote',
+              },
+              {
+                name: 'redText',
+                class: 'redText',
+              },
+              {
+                name: 'titleText',
+                class: 'titleText',
+                tag: 'h1',
+              },
+            ],
+            uploadUrl: 'v1/image',
+            uploadWithCredentials: false,
+            sanitize: true,
+            toolbarPosition: 'top',
+            toolbarHiddenButtons: [['bold', 'italic'], ['fontSize']],*/
       ...(this.schema?.options as WysiwygFieldOptions)?.editorConfig,
     };
   }
