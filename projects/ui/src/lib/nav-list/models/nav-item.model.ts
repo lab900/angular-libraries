f83b12253d98bc17af2c5555cf7caff998ffc5d1
Johan Cosemans <johan.cosemans@lab900.com>
2020-09-10 13:18:49 +0200

    Created a component for the icon model

--- projects/ui/src/lib/nav-list/models/nav-item.model.ts
+++ projects/ui/src/lib/nav-list/models/nav-item.model.ts
@@ -1,5 +1,8 @@
+import { Icon } from '@lab900/forms';
+
 export interface NavItem {
   label: string;
+  icon?: Icon;
   route?: string;
   children?: NavItem[];
 }
@@ -7,4 +10,5 @@ export interface NavItem {
 export interface NavItemGroup {
   label: string;
   items: NavItem[];
+  icon?: Icon;
 }
