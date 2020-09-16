e7249c10a400ae036c281fe2fd2d73cfc00cba9a
Dries Mellaerts <dries.mellaerts@lab900.com>
2020-09-16 12:03:53 +0200

    2 digits

--- projects/ui/src/lib/moments/components/moments/moments.component.ts
+++ projects/ui/src/lib/moments/components/moments/moments.component.ts
@@ -18,7 +18,17 @@ export class MomentsComponent {
   @Output()
   public addMoment: EventEmitter<Date> = new EventEmitter<Date>();
 
+  private digits(value: number): string {
+    return value < 10 ? `0${value}` : `${value}`;
+  }
+
   public addCurrentMoment(): void {
     this.addMoment.emit(new Date(Date.now()));
+  }
+
+  public getFormattedDate(moment: Date): string {
+    return `${this.digits(moment.getDate())}/${this.digits(moment.getMonth())}/${moment.getFullYear()} - ${this.digits(
+      moment.getHours(),
+    )}:${this.digits(moment.getMinutes())}:${this.digits(moment.getSeconds())}`;
   }
 }
