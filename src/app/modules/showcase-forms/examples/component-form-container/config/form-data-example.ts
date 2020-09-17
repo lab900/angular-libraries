8f9eeeea185ac8f3f79a4c981e3e3553eb517637
Dries Mellaerts <dries.mellaerts@lab900.com>
2020-09-17 16:05:53 +0200

    gender data removed

--- src/app/modules/showcase-forms/examples/component-form-container/config/form-data-example.ts
+++ src/app/modules/showcase-forms/examples/component-form-container/config/form-data-example.ts
@@ -2,5 +2,4 @@ export const formDataExample = {
   firstName: 'Marie',
   lastName: 'Jean',
   registration: [{ value: '06/10/1997 - 12:00:00' }, { value: '08/10/1997 - 12:00:00' }],
-  gender: 'male',
 };
