5c5a4e4004c5c3a8219f152e7a6a36b252ba2e44
Dries Mellaerts <dries.mellaerts@lab900.com>
2020-09-21 16:57:36 +0200

    Made Promise Observable & value any

--- projects/forms/src/lib/components/form-fields/select-field/select-field.component.ts
+++ projects/forms/src/lib/components/form-fields/select-field/select-field.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, HostBinding } from '@angular/core';
 import { FormComponent } from '../../../models/IFormComponent';
 import { SelectFieldOptions } from '../../../models/FormField';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'lab900-select-field',
@@ -10,7 +11,7 @@ export class SelectFieldComponent extends FormComponent<SelectFieldOptions> impl
   @HostBinding('class')
   public classList = 'lab900-form-field';
 
-  public values: { key: string; value: string }[];
+  public values: { value: any; label: string }[];
 
   public ngOnInit(): void {
     this.values = (this.options && this.options.values) || [];
@@ -20,9 +21,6 @@ export class SelectFieldComponent extends FormComponent<SelectFieldOptions> impl
   }
 
   public loadValues() {
-    this.options
-      .valuesFn()
-      .then((values) => (this.values = values))
-      .catch((err) => console.log(err));
+    this.options.valuesFn().pipe(map((value) => (this.values = value)));
   }
 }
