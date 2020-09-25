fe725c46c1984a84c93eb9ec685649163579aa63
Dries Mellaerts <dries.mellaerts@lab900.com>
2020-09-25 16:27:07 +0200

    Removed currentSelection

--- projects/ui/src/lib/page-header/components/page-header/page-header.component.ts
+++ projects/ui/src/lib/page-header/components/page-header/page-header.component.ts
@@ -1,14 +1,13 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { PageHeaderNavItem } from '../../models/page-header-nav.model';
 import { PageHeaderAction } from '../../models/page-header-actions.model';
-import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'lab900-page-header',
   templateUrl: './page-header.component.html',
   styleUrls: ['./page-header.component.scss'],
 })
-export class Lab900PageHeaderComponent implements OnInit {
+export class Lab900PageHeaderComponent {
   @Input()
   public pageTitle: string;
 
@@ -24,38 +23,11 @@ export class Lab900PageHeaderComponent implements OnInit {
   @Input()
   public data?: any;
 
-  public currentSelected: PageHeaderNavItem;
-
-  constructor(private activatedRoute: ActivatedRoute) {}
-
-  ngOnInit() {
-    // Set the initial item as selected
-    this.currentSelected = this.navItems.length > 0 ? this.navItems[0] : null;
-    for (const navItem of this.navItems) {
-      if (this.hasRequestParams(navItem.queryParams)) {
-        this.currentSelected = navItem;
-      }
-    }
-  }
-
   public getLabel(item: PageHeaderNavItem | PageHeaderAction): string {
     if (typeof item.label === 'function') {
       return item.label(this.data);
     }
     return item.label;
-  }
-
-  public hasRequestParams(params: { [key: string]: any }) {
-    const paramMap = this.activatedRoute.snapshot.queryParamMap;
-    if (!paramMap || !params) {
-      return false;
-    }
-    for (const param of Object.keys(params)) {
-      if (params[param] !== paramMap.get(param)) {
-        return false;
-      }
-    }
-    return true;
   }
 
   public getRoute(item: PageHeaderNavItem): string | void {
