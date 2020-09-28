09b6d93b4400f7d9f15593aeb9af8edff574a7f7
Dries Mellaerts <dries.mellaerts@lab900.com>
2020-09-28 12:12:53 +0200

    Start layout

--- projects/ui/src/lib/object-merger/object-merger.module.ts
+++ projects/ui/src/lib/object-merger/object-merger.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { Lab900ObjectMergerComponent } from './components/object-merger/object-merger.component';
+import { SharedModule } from '../../../../../src/app/modules/shared/shared.module';
 
 @NgModule({
   declarations: [Lab900ObjectMergerComponent],
   exports: [Lab900ObjectMergerComponent],
-  imports: [CommonModule, TranslateModule, FlexLayoutModule],
+  imports: [CommonModule, TranslateModule, FlexLayoutModule, SharedModule],
 })
 export class Lab900ObjectMergerModule {}
