badc74eb4cf3ef2dcec2f292ef67cd33f8a09bb3
Dries Mellaerts <dries.mellaerts@lab900.com>
2020-09-28 16:01:03 +0200

    ObjectMergerRow

---
+++ projects/ui/src/lib/object-merger/components/object-merger-row/object-merger-row.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lab900-object-merger-row',
+  templateUrl: './object-merger-row.component.html',
+  styleUrls: ['./object-merger-row.component.scss'],
+})
+export class Lab900ObjectMergerRowComponent {}
--- projects/ui/src/lib/object-merger/object-merger.module.ts
+++ projects/ui/src/lib/object-merger/object-merger.module.ts
@@ -4,9 +4,10 @@ import { TranslateModule } from '@ngx-translate/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { Lab900ObjectMergerComponent } from './components/object-merger/object-merger.component';
 import { SharedModule } from '../../../../../src/app/modules/shared/shared.module';
+import { Lab900ObjectMergerRowComponent } from './components/object-merger-row/object-merger-row.component';
 
 @NgModule({
-  declarations: [Lab900ObjectMergerComponent],
+  declarations: [Lab900ObjectMergerComponent, Lab900ObjectMergerRowComponent],
   exports: [Lab900ObjectMergerComponent],
   imports: [CommonModule, TranslateModule, FlexLayoutModule, SharedModule],
 })
