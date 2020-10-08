341ed998853414dc67f370a50b67f20cf1c679c3
Dries Mellaerts <dries.mellaerts@lab900.com>
2020-10-08 12:24:09 +0200

    Deployed UI && Fix setTimeout

--- projects/ui/src/lib/merger/components/merger-item/merger-item.component.ts
+++ projects/ui/src/lib/merger/components/merger-item/merger-item.component.ts
@@ -1,4 +1,14 @@
-import { AfterViewInit, Component, ComponentFactoryResolver, ComponentRef, Input, ViewChild, ViewContainerRef } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ComponentFactoryResolver,
+  ComponentRef,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
 import { MergeConfig } from '../../models/merge-config.model';
 import { isObservable, Observable, of } from 'rxjs';
 import { CustomComponent, CustomComponentAbstract } from '../../abstracts/custom-component.abstract';
@@ -7,7 +17,7 @@ import { CustomComponent, CustomComponentAbstract } from '../../abstracts/custom
   selector: 'lab900-merger-item',
   templateUrl: './merger-item.component.html',
 })
-export class Lab900MergerItemComponent<T> implements CustomComponentAbstract<T>, AfterViewInit {
+export class Lab900MergerItemComponent<T> implements CustomComponentAbstract<T>, AfterViewInit, OnChanges {
   @Input()
   public config: MergeConfig<T>;
 
@@ -23,6 +33,12 @@ export class Lab900MergerItemComponent<T> implements CustomComponentAbstract<T>,
   private customComponentRef: ComponentRef<CustomComponent<T>>;
 
   constructor(private resolver: ComponentFactoryResolver) {}
+
+  public ngOnChanges(changes: SimpleChanges): void {
+    if (changes.data && this.customComponentRef) {
+      this.customComponentRef.instance.data = this.data;
+    }
+  }
 
   public ngAfterViewInit(): void {
     if (this.config?.component) {
--- src/app/modules/showcase-ui/showcase-ui.module.ts
+++ src/app/modules/showcase-ui/showcase-ui.module.ts
@@ -35,6 +35,7 @@ const examples = [
 
 @NgModule({
   declarations: examples,
+  entryComponents: [CustomExampleComponent],
   imports: [
     CommonModule,
     ShowcaseUiRoutingModule,
