6c0e87d9a7e5b4e9d5a3146d6846d310f1d04651
Dries Mellaerts <dries.mellaerts@lab900.com>
2020-10-19 09:18:45 +0200

    Translation

--- src/app/modules/showcase-ui/examples/merger-example/merger-example.component.ts
+++ src/app/modules/showcase-ui/examples/merger-example/merger-example.component.ts
@@ -13,7 +13,7 @@ import { MergerDataExample } from './models/merger-data-example.model';
       <button mat-flat-button color="primary" (click)="mergerComponent.reset()">{{ 'reset' | translate }}</button>
     </div>
     <lab900-merger [leftObject]="exampleData[0]" [rightObject]="exampleData[1]" [schema]="exampleSchema"></lab900-merger>
-    <button style="margin-right: 1rem" mat-raised-button color="primary" (click)="showResult()">Log Result</button>
+    <button style="margin-right: 1rem" mat-raised-button color="primary" (click)="showResult()">{{ 'log_result' | translate }}</button>
   `,
 })
 export class MergerExampleComponent {
