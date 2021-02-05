15aacff9fad9991827e41d954f15cd21defda793
lab900-senn-lambrecht <senn.lambrecht@lab900.com>
2021-02-05 11:55:26 +0100

    New forms release

--- projects/forms/src/lib/components/form-dialog/form-dialog.component.ts
+++ projects/forms/src/lib/components/form-dialog/form-dialog.component.ts
@@ -1,6 +1,7 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, ViewChild } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { DialogFormData } from '../../models/dialogFormData';
+import { FormContainerComponent } from '../form-container/form-container.component';
 
 @Component({
   selector: 'lab900-form-dialog',
@@ -8,21 +9,32 @@ import { DialogFormData } from '../../models/dialogFormData';
   styleUrls: ['./form-dialog.component.scss'],
 })
 export class FormDialogComponent<T> {
+  @ViewChild(FormContainerComponent)
+  public formContainer: FormContainerComponent<T>;
+
   public dialogFormData: DialogFormData<T>;
   public loading = false;
   public error: string;
 
-  constructor(@Inject(MAT_DIALOG_DATA) private data: DialogFormData<T>, private dialogRef: MatDialogRef<FormDialogComponent<T>>) {
+  constructor(
+    @Inject(MAT_DIALOG_DATA) private data: DialogFormData<T>,
+    private dialogRef: MatDialogRef<FormDialogComponent<T>>
+  ) {
     this.dialogFormData = data;
   }
 
-  submit(item: T) {
-    this.loading = true;
-    this.error = null;
-    this.dialogFormData
-      .submit(item)
-      .catch((error) => (this.error = error))
-      .then((result) => (result ? this.dialogRef.close() : (this.error = 'Oops. An error occured.')))
-      .finally(() => (this.loading = false));
+  submit(valid: boolean, item: T) {
+    if (valid) {
+      this.loading = true;
+      this.error = null;
+      this.dialogFormData
+        .submit(item)
+        .catch((error) => (this.error = error))
+        .then((result) => (result ? this.dialogRef.close() : (this.error = 'Oops. An error occured.')))
+        .finally(() => (this.loading = false));
+    } else if (!valid && this.data?.onInvalid) {
+      this.data?.onInvalid(this.formContainer?.form, item);
+    }
   }
+
 }
--- projects/forms/src/lib/models/dialogFormData.ts
+++ projects/forms/src/lib/models/dialogFormData.ts
@@ -1,7 +1,9 @@
 import { Form } from './Form';
+import { FormGroup } from '@angular/forms';
 
 export interface DialogFormData<T> {
   schema: Form;
   data: T;
   submit: (data: T) => Promise<boolean>;
+  onInvalid: (formGroup: FormGroup, data: T) => void;
 }
