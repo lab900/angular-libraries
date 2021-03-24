1dbbe56fbdb3e5d452268b3ad1c6d032993da308
Johan Cosemans <johan.cosemans@lab900.com>
2021-03-24 21:33:31 +0100

    Added conditional example

--- src/app/modules/showcase-forms/examples/form-conditionals-example/config/form-conditionals-example.ts
+++ src/app/modules/showcase-forms/examples/form-conditionals-example/config/form-conditionals-example.ts
@@ -3,15 +3,51 @@ import { EditType, Form } from '@lab900/forms';
 export const formConditionalsExample: Form = {
   fields: [
     {
-      attribute: 'list',
-      editType: EditType.Select,
-      options: {
-        selectOptions: [
-          { label: 'one', value: 'one' },
-          { label: 'two', value: 'two' },
-          { label: 'three', value: 'three' },
-        ],
-      },
+      attribute: '',
+      editType: EditType.Row,
+      nestedFields: [
+        {
+          attribute: 'country',
+          editType: EditType.Select,
+          options: {
+            colspan: 6,
+            selectOptions: [
+              { label: 'belgium', value: 'BEL' },
+              { label: 'france', value: 'FRA' },
+              { label: 'germany', value: 'GER' },
+              { label: 'hide language', value: 'HIDE' },
+            ],
+          },
+        },
+        {
+          attribute: 'languages',
+          editType: EditType.Select,
+          options: {
+            colspan: 6,
+          },
+          conditions: [
+            {
+              dependOn: 'country',
+              conditionalOptions: (country: string) => {
+                console.log(country);
+                switch (country) {
+                  case 'BEL':
+                    return [
+                      { label: 'Dutch', value: 'NL' },
+                      { label: 'French', value: 'FR' },
+                      { label: 'German', value: 'DE' },
+                    ];
+                  case 'FRA':
+                    return [{ label: 'French', value: 'FR' }];
+                  case 'GER':
+                    return [{ label: 'German', value: 'DE' }];
+                }
+              },
+              hideIfEquals: (country: string) => country === 'HIDE',
+            },
+          ],
+        },
+      ],
     },
   ],
 };
