d6ed1ed811c59c31a7d7809a2aaf12d002e37489
lab900-winand-vandenbergh <winand.vandenbergh@lab900.com>
2021-03-30 17:07:59 +0200

    Forms release 2.3.7

--- projects/forms/src/lib/components/image-preview-modal/image-preview-modal.component.ts
+++ projects/forms/src/lib/components/image-preview-modal/image-preview-modal.component.ts
@@ -2,6 +2,10 @@ import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Image } from '../../models/Image';
 
+interface DialogData {
+  image: Image;
+}
+
 @Component({
   selector: 'lab900-image-preview-modal',
   templateUrl: './image-preview-modal.component.html',
@@ -11,7 +15,7 @@ export class ImagePreviewModalComponent {
   public image: Image;
   public error = true;
 
-  constructor(@Inject(MAT_DIALOG_DATA) private data: { image: Image }) {
+  constructor(@Inject(MAT_DIALOG_DATA) private data: DialogData) {
     this.image = data.image;
   }
 }
