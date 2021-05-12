f6b857c2989ecbcd9722a2f5d2d1cf79cee284db
lab900-winand-vandenbergh <winand.vandenbergh@lab900.com>
2021-05-12 16:37:12 +0200

    Added debounce to autocomplete input change to prevent too many backend calls

--- projects/forms/src/lib/components/form-fields/autocomplete-field/autocomplete-field.component.ts
+++ projects/forms/src/lib/components/form-fields/autocomplete-field/autocomplete-field.component.ts
@@ -1,16 +1,22 @@
-import { Component, HostBinding } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostBinding, ViewChild } from '@angular/core';
 import { FormComponent } from '../../../models/IFormComponent';
 import { AutocompleteOptions, ValueLabel } from '../../../models/FormField';
-import { Observable, isObservable, of } from 'rxjs';
+import { Observable, isObservable, of, fromEvent } from 'rxjs';
 import { TranslateService } from '@ngx-translate/core';
+import { debounceTime, distinctUntilChanged, map, switchMap, tap } from 'rxjs/operators';
+import { FormControl } from '@angular/forms';
+import { logger } from 'codelyzer/util/logger';
 
 @Component({
   selector: 'lab900-autocomplete-field',
   templateUrl: './autocomplete-field.component.html',
 })
-export class AutocompleteFieldComponent extends FormComponent<AutocompleteOptions> {
+export class AutocompleteFieldComponent extends FormComponent<AutocompleteOptions> implements AfterViewInit {
   @HostBinding('class')
   public classList = 'lab900-form-field';
+
+  @ViewChild('input')
+  public autoCompleteInput: ElementRef;
 
   public filteredOptions: Observable<ValueLabel[]>;
 
@@ -18,8 +24,20 @@ export class AutocompleteFieldComponent extends FormComponent<AutocompleteOption
     super(translateService);
   }
 
-  public inputChanged($event: Event): void {
-    const res = this.options.autocompleteOptions(($event.target as any).value, this.fieldControl);
-    this.filteredOptions = isObservable(res) ? res : of(res);
+  public ngAfterViewInit(): void {
+    super.ngAfterViewInit();
+    this.initFilteredOptionsListener();
+  }
+
+  private initFilteredOptionsListener(): void {
+    const debounce: number = this.options.debounceTime ?? (isObservable(this.options.autocompleteOptions('', this.fieldControl)) ? 300 : 0);
+    this.filteredOptions = fromEvent(this.autoCompleteInput.nativeElement, 'keyup').pipe(
+      debounceTime(debounce),
+      distinctUntilChanged(),
+      switchMap((input: Event) => {
+        const res = this.options.autocompleteOptions((input.target as any).value, this.fieldControl);
+        return isObservable(res) ? res : of(res);
+      }),
+    );
   }
 }
