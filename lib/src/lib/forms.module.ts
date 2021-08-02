faf90b4794a21b995b2a10ee54d82d62f5874f2b
Frederico Teixeira <frederico.teixeira@lab900.com>
2021-08-02 13:24:42 +0200

    Merge branch 'dev' into feature/add-slide-toggle

--- lib/src/lib/forms.module.ts
+++ lib/src/lib/forms.module.ts
@@ -2,7 +2,10 @@ import { ModuleWithProviders, NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldModule } from '@angular/material/form-field';
+import {
+  MAT_FORM_FIELD_DEFAULT_OPTIONS,
+  MatFormFieldModule,
+} from '@angular/material/form-field';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
@@ -27,7 +30,11 @@ import { SelectFieldComponent } from './components/form-fields/select-field/sele
 import { MatSelectModule } from '@angular/material/select';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { CheckboxFieldComponent } from './components/form-fields/checkbox-field/checkbox-field.component';
-import { defaultFormModuleSettings, Lab900FormModuleSettings, LAB900_FORM_MODULE_SETTINGS } from './models/Lab900FormModuleSettings';
+import {
+  defaultFormModuleSettings,
+  Lab900FormModuleSettings,
+  LAB900_FORM_MODULE_SETTINGS,
+} from './models/Lab900FormModuleSettings';
 import { TextareaFieldComponent } from './components/form-fields/textarea-field/textarea-field.component';
 import { FormRowComponent } from './components/form-row/form-row.component';
 import { RepeaterFieldComponent } from './components/form-fields/repeater-field/repeater-field.component';
@@ -48,7 +55,11 @@ import { MatChipsModule } from '@angular/material/chips';
 import { NgxMaskModule } from 'ngx-mask';
 import { ReadonlyFieldComponent } from './components/form-fields/readonly-field/readonly-field.component';
 import { DateRangeFieldComponent } from './components/form-fields/date-range-field/date-range-field.component';
-import { NgxMatDatetimePickerModule, NgxMatNativeDateModule, NgxMatTimepickerModule } from '@angular-material-components/datetime-picker';
+import {
+  NgxMatDatetimePickerModule,
+  NgxMatNativeDateModule,
+  NgxMatTimepickerModule,
+} from '@angular-material-components/datetime-picker';
 import { DateTimeFieldComponent } from './components/form-fields/date-time-field/date-time-field.component';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { FilePreviewFieldComponent } from './components/form-fields/file-preview-field/file-preview-field.component';
@@ -61,6 +72,8 @@ import { MatMenuModule } from '@angular/material/menu';
 import { LanguagePickerComponent } from './components/language-picker/language-picker.component';
 import { MultiLangFieldControlComponent } from './components/form-fields/multi-lang-input/multi-lang-field-control/multi-lang-field-control.component';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSelectInfiniteScrollModule } from 'ng-mat-select-infinite-scroll';
+import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
 
 const customFields = [
   UnknownFieldComponent,
@@ -133,12 +146,24 @@ const customFields = [
     MatTooltipModule,
     MatSlideToggleModule,
     MatMenuModule,
+    MatSelectInfiniteScrollModule,
+    NgxMatSelectSearchModule,
   ],
-  exports: [Lab900Form, FormDialogDirective, AuthImageDirective, AutofocusDirective],
+  exports: [
+    Lab900Form,
+    FormDialogDirective,
+    AuthImageDirective,
+    AutofocusDirective,
+  ],
 })
 export class Lab900FormsModule {
-  public static forRoot(settings: Lab900FormModuleSettings = defaultFormModuleSettings): ModuleWithProviders<FormsModule> {
-    const formSetting: Lab900FormModuleSettings = { ...defaultFormModuleSettings, ...settings };
+  public static forRoot(
+    settings: Lab900FormModuleSettings = defaultFormModuleSettings
+  ): ModuleWithProviders<FormsModule> {
+    const formSetting: Lab900FormModuleSettings = {
+      ...defaultFormModuleSettings,
+      ...settings,
+    };
     return {
       ngModule: Lab900FormsModule,
       providers: [
